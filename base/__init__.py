d2a1bc3f1446982b6ee78594c18ce1dc9b70becd
YinxiaCao <542799173@qq.com>
2022-04-23 13:23:46 +0800

    Add files via upload

---
+++ base/__init__.py
@@ -0,0 +1,11 @@
+from .model import SegmentationModel, ClassificationModel
+
+from .modules import (
+    Conv2dReLU,
+    Attention,
+)
+
+from .heads import (
+    SegmentationHead,
+    ClassificationHead,
+)
